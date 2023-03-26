89de449a1bb4b12c7ad4cb45a83edbccda919148
smck0583 <smck0583@uni.sydney.edu.au>
2023-03-26 20:06:50 +1100

    Navigation through Linking

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import { Routes, Route } from "react-router-dom";
 import AllMeetupsPage from "./Pages/AllMeetups";
 import NewMeetupPage from "./Pages/NewMeetup";
 import FavoritesPage from "./Pages/Favorites";
+import MainNavigation from './Components/Layout/MainNavigation';
 
 function App() {
   return (
-    <div>
+    <div>  
+      <MainNavigation />
       <Routes>
-        <Route path='/' exact element={<AllMeetupsPage />} />
-        <Route path='/new-meetup' element={<NewMeetupPage />} />
-        <Route path='/favorites' element={<FavoritesPage />} />
+        <Route path="/" exact element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
     </div>
   );
