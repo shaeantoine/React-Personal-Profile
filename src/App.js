4ffa553042c672a7fe5f9bb52f454b6b0941b6a5
smck0583 <smck0583@uni.sydney.edu.au>
2023-03-26 19:47:19 +1100

    Updated Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+//import { Route, Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
+import AllMeetupsPage from "./Pages/AllMeetups";
+import NewMeetupPage from "./Pages/NewMeetup";
+import FavoritesPage from "./Pages/Favorites";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path='/' exact element={<AllMeetupsPage />} />
+        <Route path='/new-meetup' element={<NewMeetupPage />} />
+        <Route path='/favorites' element={<FavoritesPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
